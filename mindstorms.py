aeb5e550b44e83c3de9454ddf661386fe50b2836
alvertis <konstantinos.alvertis@gmail.com>
2018-04-11 01:22:22 +0300

    customize color of turtle

--- mindstorms.py
+++ mindstorms.py
@@ -3,9 +3,12 @@ import turtle #this moves around and draw staff - Introduced to
 
 def draw_square():
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("gray")
     
     brad = turtle.Turtle() # to grab a turtle
+    brad.shape("square")
+    brad.color("blue")
+    brad.speed(1)
     for x in range(0,4):
         brad.forward(100)      # move 100 steps forward
         brad.right(90)         # turn right 90 degrees
