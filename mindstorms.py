815e643b50b3cff445d896aaf7de12d4a6147c8d
alvertis <konstantinos.alvertis@gmail.com>
2018-04-11 01:53:30 +0300

    add triangle

--- mindstorms.py
+++ mindstorms.py
@@ -1,10 +1,15 @@
 import turtle #this moves around and draw staff - Introduced to
 #help kids learn programming easily
 
-def draw_square():
+def draw_shapes():
     window = turtle.Screen()
     window.bgcolor("gray")
-    
+    draw_square()
+    draw_circle()
+    draw_triangle()
+    window.exitonclick()
+
+def draw_square():  
     brad = turtle.Turtle() # to grab a turtle
     brad.shape("square")
     brad.color("blue")
@@ -13,10 +18,20 @@ def draw_square():
         brad.forward(100)      # move 100 steps forward
         brad.right(90)         # turn right 90 degrees
 
+
+def draw_circle():
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("yellow")
     angie.circle(100)
-    window.exitonclick()
 
-draw_square()
+def draw_triangle():
+    joe = turtle.Turtle()
+    joe.shape("triangle")
+    joe.color("white")
+    joe.speed(1)
+    for x in range(0,3):
+        joe.forward(100) 
+        joe.left(120)
+
+draw_shapes()
