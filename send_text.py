11b9aea519a1aa25fca793c5caa804554687c7cc
alvertis <konstantinos.alvertis@gmail.com>
2018-04-11 04:35:36 +0300

    change the import and from fields

--- send_text.py
+++ send_text.py
@@ -1,11 +1,11 @@
-from twilio.rest import Client
+from twilio import rest
 
 # Your Account SID from twilio.com/console
 account_sid = "AC695595632f22a06337abc7b01d306a6e"
 # Your Auth Token from twilio.com/console
 auth_token  = "b78bd556bc82598ffedd7cae85925095"
 
-client = Client(account_sid, auth_token)
+client = rest.Client(account_sid, auth_token)
 
 message = client.messages.create(
     to="+306933127761", 
